32dddeb5c9ccc1506bb72d45d77eed13f2ebf308
Maatouf-Saad <saad_maatouf@hotmail.fr>
2023-02-12 15:00:42 +0100

    Promise Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,9 +12,9 @@ export class AboutComponent implements OnInit {
   leaders: Leader[];
 
   constructor( private leaderService: LeaderService) { }
-
+  
   ngOnInit() {
-    this.leaders = this.leaderService.getLeaders();
+    this.leaderService.getLeaders().then( leaders => this.leaders = leaders)
   }
 
 }
